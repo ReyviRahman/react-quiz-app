83e72e8d77b5beb8de33db65d8f19078dfbda070
Reyvi Rahman <reyvirahman@gmail.com>
2024-11-18 05:55:35 +0700

    feature get soal

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import Header from './Header'
 import Main from './Main'
 import axios from 'axios'
+import Loader from './Loader'
+
+const initialState = {
+  questions: [],
+  status: 'loading'
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'dataReceived': 
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready'
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error'
+      };
+    default:
+      throw new Error('Action Unknown');
+  }
+}
 
 const App = () => {
+  const [{question, status}, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    const fetchDate = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get('')
-        
+        const response = await axios.get('http://localhost:8000/questions')
+        dispatch({type: 'dataReceived', payload: response.data})
       } catch (error) {
-        
+        dispatch({type: 'dateFailed'})
       }
     }
+
+    fetchData()
   }, [])
 
   return (
     <div className='app'>
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
       </Main>
     </div>
   )
