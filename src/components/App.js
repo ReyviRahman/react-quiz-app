4b7030f70e33e228f03c3b6d48ccf68215e501b4
Reyvi Rahman <reyvirahman@gmail.com>
2024-11-19 09:20:49 +0700

    Adding restart Quiz Feature

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,19 @@ import Loader from './Loader'
 import Error from './Error'
 import StartScreen from './StartScreen'
 import Question from './Question'
+import NextButton from './NextButton'
+import Progress from './Progress'
+import FinishScreen from './FinishScreen'
+import Footer from './Footer'
+import Timer from './Timer'
 
 const initialState = {
   questions: [],
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0
 }
 
 const reducer = (state, action) => {
@@ -31,15 +39,42 @@ const reducer = (state, action) => {
         ...state,
         status: 'active'
       }
+    case 'newAnswer':
+      const question = state.questions.at(state.index)
+      return {
+        ...state,
+        answer: action.payload,
+        points: action.payload === question.correctOption ? state.points + question.points : state.points
+      }
+    case 'nextQuestion':
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null
+      }
+    case 'finish':
+      return {
+        ...state,
+        status: 'finished',
+        highscore: 
+          state.points > state.highscore ? state.points : state.highscore 
+      }
+    case 'restart': 
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: 'ready'
+      }
     default:
       throw new Error('Action Unknown');
   }
 }
 
 const App = () => {
-  const [{questions, status, index}, dispatch] = useReducer(reducer, initialState)
+  const [{questions, status, index, answer, points, highscore}, dispatch] = useReducer(reducer, initialState)
 
   const numQuestions = questions.length
+  const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -61,7 +96,21 @@ const App = () => {
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
         {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === 'active' && <Question question = {questions[index]}/>}
+        {status === 'active' && (
+          <>
+            <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer} />
+            <Question 
+            question = {questions[index]} 
+            dispatch={dispatch} 
+            answer={answer} 
+            />
+            <Footer>
+              <Timer />
+              <NextButton dispatch={dispatch} answer={answer} numQuestions={numQuestions} index={index} />
+            </Footer>
+          </>
+        )}
+        {status === 'finished' && <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore} dispatch={dispatch} /> }
       </Main>
     </div>
   )
---
+++ src/components/FinishScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FinishScreen = ({points, maxPossiblePoints, highscore, dispatch}) => {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  return (
+    <>
+      <p className='result'>
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className='highscore'>(Highscore: {highscore} points)</p>
+      <button className='btn btn-ui' onClick={() => dispatch({ type: 'restart'})}>Restart Quiz</button>
+    </>
+  )
+}
+
+export default FinishScreen
